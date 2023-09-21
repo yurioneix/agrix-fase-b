ced04a687e6241fff3b6688bcda5fe7cfb2c8bec
yurioneix <yurioneix18@gmail.com>
2023-09-21 16:48:56 -0300

    Requisito 1 - migra fase código da fase A para fase B

--- src/main/java/com/betrybe/agrix/ebytr/staff/repository/PersonRepository.java
+++ src/main/java/com/betrybe/agrix/ebytr/staff/repository/PersonRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 /**
  * Person JPA repository.
  */
+
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
   Optional<Person> findByUsername(String username);
